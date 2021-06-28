7874d66afb630a1c306fcad97b1adf0cb12b0b5e
Omar Afifi <omarafifi@windowslive.com>
2021-06-28 22:28:42 +0200

    Update HomeAdmin.js

--- src/components/HomeAdmin.js
+++ src/components/HomeAdmin.js
@@ -1,6 +1,8 @@
 import React, { Fragment } from 'react';
 import { MyContext } from '../App';
 import Header from './Header';
+import AdminArt from './AdminArt';
+import AdminUser from './AdminUser';
 
 import {
   Flex,
@@ -31,28 +33,28 @@ import {
 
 const HomeAdmin = props => {
   const initialState = {
-    user_image: 'assets/nav/users/user_w@1x.svg',
-    art_image: 'assets/nav/art/supervised_user_circle.svg',
+    user_image: 'assets/nav/users/user@1x.svg',
+    art_image: 'assets/nav/art/supervised_user_circle@1x.svg',
   };
   const [data, setData] = React.useState(initialState);
 
   const setTabImage = index => {
-      console.log('index ', index);
-      let mydata;
+    console.log('index ', index);
+    let mydata;
     switch (index) {
       case 0:
-          console.log('case 0')
+        console.log('case 0');
         mydata = {
-          user_image: 'assets/nav/users/user_w@1x.svg',
-          art_image: 'assets/nav/art/supervised_user_circle.svg',
+          user_image: 'assets/nav/users/user@1x.svg',
+          art_image: 'assets/nav/art/supervised_user_circle@1x.svg',
         };
         setData(mydata);
         break;
       case 1:
-          console.log('case 1')
+        console.log('case 1');
         mydata = {
-          user_image: 'assets/nav/users/user@1x.svg',
-          art_image: 'assets/nav/art/supervised_user_circle@1x.svg',
+          user_image: 'assets/nav/users/user_w@1x.svg',
+          art_image: 'assets/nav/art/supervised_user_circle.svg',
         };
         setData(mydata);
         break;
@@ -64,19 +66,19 @@ const HomeAdmin = props => {
   return (
     <Tabs isLazy orientation="vertical" onChange={index => setTabImage(index)}>
       <TabList>
-        <Tab>
+        <Tab _selected={{ bg: 'blue.500' }}>
           <img src={data.art_image} alt="" />
         </Tab>
-        <Tab>
+        <Tab _selected={{ bg: 'blue.500' }}>
           <img src={data.user_image} alt="" />
         </Tab>
       </TabList>
       <TabPanels>
         <TabPanel>
-          <p>one!</p>
+          <AdminArt/>
         </TabPanel>
         <TabPanel>
-          <p>two!</p>
+          <AdminUser/>
         </TabPanel>
       </TabPanels>
     </Tabs>
